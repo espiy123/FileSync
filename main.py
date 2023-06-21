f0ec13b25d6d916f305de88a769134bb885ac242
espiy123 <filip.pieprzyk@gmail.com>
2023-06-21 21:22:40 +0200

    GUI implementation

--- main.py
+++ main.py
@@ -1,63 +1,22 @@
-import os
-import glob
-import shutil
-# from tkinter import *
-# test2
+from PySide2.QtCore import QSize, Qt
+from PySide2.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
+from sort import organise
+import sys
 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
+        self.setWindowTitle("FileFlow")
+        self.setFixedSize(QSize(700, 500))
+        
+        button = QPushButton("Ogranise")
+        button.setFixedSize(QSize(100,100))
+        button.clicked.connect(organise)
+        self.setCentralWidget(button)
 
-dirNames = ["Audio", "Documents", "Videos", "Images", "Executables", "Archives", "Other"]
-
-for x in dirNames:
-    if os.path.exists(x):
-        print(f"Warning! Directory {x} already exists! Do you want to continue? Y/N")
-        while True:
-            temp = input()
-            if temp.lower() == "y":
-                break
-            elif temp.lower() == "n":
-                raise Exception("Directory already exists!")
-            else:
-                print("Choose the correct answer!")
-                continue
-    else:
-        os.makedirs(x)
-
-audioExtensions = ["mp3", "waw", "m4a", "flac", "wma", "aac"]
-documentsExtensions = ["odt", "pdf", "xls", "xlsx", "ods", "ppt", "pptx", "txt", "epub", "mobi" ,"doc", "docx", "rtf"]
-videosExtensions = ["mp4", "webm", "flv", "vob", "avi", "amv", "mpg", "mov"]
-imagesExtensions = ["jpeg", "jpg", "tiff", "png", "bmp", "xcf", "psd", "flif", "gif"]
-exeExtensions = ["exe", "msi"]
-archivesExtensions = ["7z", "zip", "rar", "bz2", "gz", "ace", "tar"]
-
-number = 0
-for file in glob.glob("*.*"):
-    temp = file.split('.')
-    
-
-    if temp[0] == "Organiser" and temp[1] == "exe":
-        continue
-    elif temp[1] in audioExtensions:
-        shutil.move(file, "Audio")
-        number += 1
-    elif temp[1] in documentsExtensions:
-        shutil.move(file, "Documents")
-        number += 1
-    elif temp[1] in videosExtensions:
-        shutil.move(file, "Videos")
-        number += 1
-    elif temp[1] in imagesExtensions:
-        shutil.move(file, "Images")
-        number += 1
-    elif temp[1] in exeExtensions:
-        shutil.move(file, "Executables")
-        number += 1
-    elif temp[1] in archivesExtensions:
-        shutil.move(file, "Archives")
-        number += 1
-    elif temp[1] != "py":
-        shutil.move(file, "Other")
-        number += 1
-
-print(f"replacement finished! Moved {number} file(s).")
-input()
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec_()
+sys.exit(0)
---
+++ sort.py
@@ -0,0 +1,63 @@
+import os
+import glob
+import shutil
+# from tkinter import *
+# test2
+
+
+
+dirNames = ["Audio", "Documents", "Videos", "Images", "Executables", "Archives", "Other"]
+
+def organise():
+    for x in dirNames:
+        if os.path.exists(x):
+            print(f"Warning! Directory {x} already exists! Do you want to continue? Y/N")
+            while True:
+                temp = input()
+                if temp.lower() == "y":
+                    break
+                elif temp.lower() == "n":
+                    raise Exception("Directory already exists!")
+                else:
+                    print("Choose the correct answer!")
+                    continue
+        else:
+            os.makedirs(x)
+
+    audioExtensions = ["mp3", "waw", "m4a", "flac", "wma", "aac"]
+    documentsExtensions = ["odt", "pdf", "xls", "xlsx", "ods", "ppt", "pptx", "txt", "epub", "mobi" ,"doc", "docx", "rtf"]
+    videosExtensions = ["mp4", "webm", "flv", "vob", "avi", "amv", "mpg", "mov"]
+    imagesExtensions = ["jpeg", "jpg", "tiff", "png", "bmp", "xcf", "psd", "flif", "gif"]
+    exeExtensions = ["exe", "msi"]
+    archivesExtensions = ["7z", "zip", "rar", "bz2", "gz", "ace", "tar"]
+
+    number = 0
+    for file in glob.glob("*.*"):
+        temp = file.split('.')
+        
+
+        if temp[0] == "Organiser" and temp[1] == "exe":
+            continue
+        elif temp[1] in audioExtensions:
+            shutil.move(file, "Audio")
+            number += 1
+        elif temp[1] in documentsExtensions:
+            shutil.move(file, "Documents")
+            number += 1
+        elif temp[1] in videosExtensions:
+            shutil.move(file, "Videos")
+            number += 1
+        elif temp[1] in imagesExtensions:
+            shutil.move(file, "Images")
+            number += 1
+        elif temp[1] in exeExtensions:
+            shutil.move(file, "Executables")
+            number += 1
+        elif temp[1] in archivesExtensions:
+            shutil.move(file, "Archives")
+            number += 1
+        elif temp[1] != "py":
+            shutil.move(file, "Others")
+            number += 1
+
+    print(f"Replacement finished! Moved {number} file(s).")
